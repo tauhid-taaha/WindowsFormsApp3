02b0b82955861b826195ca8b8722344eb8d2a366
TAUHID TAAHA <tauhidtaaha7@gmail.com>
2023-03-11 23:23:13 +0600

    BG CHANGED

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -57,12 +57,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Location = new System.Drawing.Point(273, 105);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(214, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
