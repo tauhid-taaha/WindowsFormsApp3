1e05642ff3ad2693cd7ec91c872a397722b1451b
TAUHID TAAHA <tauhidtaaha7@gmail.com>
2023-03-11 23:20:18 +0600

    Add project files.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double celsius=Convert.ToInt32(textBox1.Text);
+            double a = ((celsius * 9) / 5) + 32;
+            textBox2.Text = a.ToString();
+        }
+    }
+}
